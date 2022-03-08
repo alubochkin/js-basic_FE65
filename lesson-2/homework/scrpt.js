70f40cb69fa99145d51642c66be437a3d91f786d
vladislav-tarasov-tms <vladtarasov306@gmail.com>
2022-03-08 11:40:49 +0300

    added lesson-2

---
+++ lesson-2/homework/scrpt.js
@@ -0,0 +1,44 @@
+/*
+    Создайте массив с десятью случайными элементами от 0 до 100;
+    Создать функцию с именем replacer, которая будет заменять все “0” на строку “zero”;
+    Функция должна возвращать новый массив
+    Пример: ["1zero2", 53, "2zero", 18, 22, "1zerozero", 43, 57, "5zero", 1];
+
+    * для рандомного числа используйте Math.floor(Math.random() * 100)
+
+    РЕШАТЬ ЧЕРЕЗ reduce
+*/
+
+/*
+    Дан массив [1,2,3,1,2,3,5,4,6,7,3,2,1,2,3,5,3,3,1,2,3,4,1]
+    Создать массив из элементов которые встречаются только один раз
+
+    РЕШАТЬ ЧЕРЕЗ reduce
+*/
+
+/*
+    Реализовать CRUD для объекта todo
+
+    в объекте todo есть поле list в котором должны храниться наши задания
+      getTodo(id) возвращает задание по id
+      createTodo(title, description) создаёт новое задание и добавлят в массив
+      updateTodo(id, newTodo) редактирует задание по id
+      deleteTodo(id) удаляет задание по id
+*/
+
+const generateId = (x = 999) => Math.random() * x;
+
+const testTodo = {
+  id: generateId(),
+  title: "Название задания",
+  description: null,
+  completed: false,
+};
+
+const todo = {
+  list: [testTodo],
+  getTodo(id) {},
+  createTodo(title, description) {},
+  updateTodo(id, newTodo) {},
+  deleteTodo(id) {},
+};
