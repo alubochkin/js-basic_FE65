1ed693583865e2d777e8059c6c27b36c55cd12ad
Ivan Kiselkov <ivan131100@icloud.com>
2022-03-08 16:36:22 +0300

    added lesson 1 kiselkov

--- lesson-1/classwork/script.js
+++ lesson-1/classwork/script.js
@@ -5,10 +5,30 @@
     Пример: [1,2,3] => [1,2,3,1,2,3] 
 */
 
+function duplicate(args) {
+  let res = [...args, ...args];
+
+  console.log(res);
+  return res;
+}
+
+duplicate([1, 2, 3]);
+
 /*
     Создайте массив с 10 уникальными числами
     * для рандомного числа используйте Math.floor(Math.random() * 100)
 */
+
+const array = [];
+
+while (array.length < 10) {
+  const randomNum = Math.floor(Math.random() * 100);
+  if (!array.includes(randomNum)) {
+    array.push(randomNum);
+  }
+}
+
+console.log(array);
 
 /*
     Создать функцию с именем getBiggest
@@ -16,14 +36,36 @@
     Функция должна уметь работать с разным количетвом параметров
 */
 
-/*
+function getBiggest(...args) {
+  let biggest = -Infinity;
+  for (let i = 0; i < args.length; i++) {
+    if (args[i] > biggest) {
+      biggest = args[i];
+    }
+  }
+
+  console.log(biggest);
+  return biggest;
+}
+
+getBiggest(1, 3, 6, 4, 9);
+
+/* ??
    Создать функцию с именем isAnagram
    Написать код функции проверки анаграммы
    isAnagram('пасечник','песчинка'); // true
    isAnagram('xyz','xyzx'); // false
 */
 
-/*
+function isAnagram(str1, str2) {
+  str1 = "пасечник";
+  str2 = "песчинка";
+  let arrStr1 = str1.split("");
+  let arrStr2 = str2.split("");
+}
+isAnagram();
+
+/* 
     Создайте массив с десятью случайными элементами от 0 до 100;
     Создать функцию с именем replacer, которая будет заменять все “0” на строку “zero”;
     Функция должна возвращать новый массив
@@ -31,3 +73,31 @@
 
     * для рандомного числа используйте Math.floor(Math.random() * 100)
 */
+
+const replacer = (arr) => {
+  let array = [];
+
+  const format = (str) => {
+    let str1 = "";
+
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] === "0") {
+        str1 += "zero";
+      } else {
+        str1 += str[i];
+      }
+    }
+
+    return str1;
+  };
+
+  arr.forEach((item) => {
+    if (String(item).includes("0")) {
+      array.push(format(String(item)));
+    } else {
+      array.push(item);
+    }
+  });
+
+  return array;
+};
--- lesson-1/homework/script.js
+++ lesson-1/homework/script.js
@@ -2,16 +2,56 @@
     1)
     Дана строка 'sOMe sTrING in lOWERcaSE'
     Создать строку вида 'SoMe sTrInG In lOwErCaSe'
+*/
 
+const strSome = "sOMe sTrING in lOWERcaSE";
+let strSome1 = "";
+
+for (let i = 0; i < str.length; i++) {
+  if (i % 2 === 0) {
+    strSome1 += strSome[i].toUpperCase();
+  } else {
+    strSome1 += strSome[i].toLowerCase();
+  }
+}
+
+console.log(strSome1);
+
+/*
     2)
     Дан массив [1,2,3,1,2,3,5,4,6,7,3,2,1,2,3,5,3,3,1,2,3,4,1]
     Создать массив из элементов которые встречаются только один раз
+*/
 
+const arrOnce = [1, 2, 3, 1, 2, 3, 5, 4, 6, 7, 3, 2, 1, 2, 3, 5, 3, 3, 1, 4];
+let arrOnce1 = [];
+
+for (let i = 0; i < arr.length; i++) {
+  if (
+    arrOnce.indexOf(arrOnce[i]) === i &&
+    arrOnce.indexOf(arrOnce[i], i + 1) === -1
+  ) {
+    arrOnce1.push(arrOnce[i]);
+  }
+}
+
+console.log(arrOnce1);
+
+/*
     3)
     Создать функцию с именем isPalindrome
     Проверить является ли строка палиндромом
     Пример палиндромов: 'доход', 'шалаш'
+*/
 
+function isPalindrome(str) {
+  str = str.toLowerCase();
+  return str === str.split("").reverse().join("") ? true : false;
+}
+
+console.log(isPalindrome("доход"));
+
+/*
     4)
     Дан обьект users
     В  окно prompt необходимо ввести имя пользователя
@@ -40,3 +80,17 @@ const users = [
     email: "Nathan@yesenia.net",
   },
 ];
+
+const searchUser = (someStr) => {
+  let lowerStr = someStr.toLowerCase();
+  for (let key in users) {
+    if (users[key].name.toLowerCase().includes(lowerStr)) {
+      console.log(`
+        user name: ${users[key].name}
+        user email: ${users[key].email}
+      `);
+    }
+  }
+};
+
+searchUser(prompt("необходимо ввести имя пользователя"));
