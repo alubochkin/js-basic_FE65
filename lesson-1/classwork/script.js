ae6f2da073fb246b26a669c42319fdff7db1655b
Anna Gruzd <ann.gruzd97.ag@gmail.com>
2022-03-08 13:14:24 +0300

    added classwork

--- lesson-1/classwork/script.js
+++ lesson-1/classwork/script.js
@@ -4,24 +4,58 @@
     Функция должна возратить удвоенный массив
     Пример: [1,2,3] => [1,2,3,1,2,3] 
 */
+const duplicate = (arr) => [...arr, ...arr];
+console.log(duplicate([1, 3, 5]));
 
 /*
     Создайте массив с 10 уникальными числами
     * для рандомного числа используйте Math.floor(Math.random() * 100)
 */
+function random() {
+  let arr = [];
+  while (arr.length !== 10) {
+    let num = Math.floor(Math.random() * 11);
+    if (!arr.includes(num)) {
+      arr.push(num);
+    }
+  }
+  console.log(arr);
+}
+random();
 
 /*
     Создать функцию с именем getBiggest
     Функция должна возвращать самое большое число из переданных параметров
     Функция должна уметь работать с разным количетвом параметров
 */
-
+function getBiggest(...args) {
+  let max = -Infinity;
+  for (let i = 0; i < args.length; i++) {
+    if (args[i] > max) {
+      max = args[i];
+    }
+  }
+  console.log(max);
+}
+getBiggest(1, 2, 3, 4, 12, 65, 890);
 /*
    Создать функцию с именем isAnagram
    Написать код функции проверки анаграммы
    isAnagram('пасечник','песчинка'); // true
    isAnagram('xyz','xyzx'); // false
 */
+function isAnagram(str1, str2) {
+  if (str1.length != str2.length) {
+    return false;
+  }
+  let str1Arr = str1.split("").sort().join();
+  let str2Arr = str2.split("").sort().join();
+  console.log(str1Arr, str2Arr);
+  let result = str1Arr === str2Arr;
+  return console.log(result);
+}
+
+isAnagram("helloy", "ohelld");
 
 /*
     Создайте массив с десятью случайными элементами от 0 до 100;
@@ -31,3 +65,21 @@
 
     * для рандомного числа используйте Math.floor(Math.random() * 100)
 */
+function replacer() {
+  const arr = [];
+  while (arr.length !== 10) {
+    let num = Math.floor(Math.random() * 100);
+    if (!arr.includes(num)) {
+      arr.push(num);
+    }
+  }
+  console.log(arr);
+  const newArr = arr.map((num) => {
+    return num.toString().includes("0")
+      ? num.toString().replace(/0/g, "zero")
+      : num;
+  });
+  console.log(newArr);
+  return newArr;
+}
+replacer();
