d3b583a73900fffd7069bbd74b03d96625fa3418
Artur <kuchinski21@inbox.ru>
2022-03-10 19:15:36 +0300

    kuchinski-lesson-2-homework

--- lesson-2/classwork/script.js
+++ lesson-2/classwork/script.js
@@ -8,6 +8,20 @@
     решить через рекурсию
 */
 
+let timer = (a, b) => {
+    if (a === b) {
+        console.log('таймер остановился');
+        return
+    }
+    setTimeout(() => {
+        console.log(a)
+
+        timer(a + 1, b);
+    }, 1000)
+}
+timer(1, 5)
+
+
 /*
     1. timer(a , b)
     2. сравнить a, b если === return
@@ -18,7 +32,12 @@
    Создать функцию с именем compact
    Функция должна очищать массив от значений: false, undefined, "", 0, null.
 */
+let compact = (args) => {
+    let array = args.filter((item) => Boolean(item))
+    return array
+}
 
+console.log(compact([null, 7, false, 3]));
 /*
     Создать объект user вида 
     {
@@ -37,9 +56,50 @@
 
     МЕТОДЫ ДОЛЖНЫ РАБОТАТЬ С this
 */
+let user = {
+    name: 'Artur',
+    lastName: 'Kuchinski',
+    age: '29',
+    knowledge: [],
+    hobbies: [],
+    addKnowledge(newKnowledge) {
+        this.knowledge.push(newKnowledge)
+    },
+    showKnowledge() {
+        console.log(this.knowledge);
+    },
+    addHobby(newHobby) {
+        this.hobbies.unshift(newHobby)
+    },
+    showHobby() {
+        console.log(this.hobbies);
+    },
+    clearKnowledge() {
+        this.knowledge = [];
+    },
+    clearHobbies() {
+        this.hobbies = [];
+    },
+    showInfo() {
+        const info = `${this.name} ${this.lastName} ${this.age} ${this.knowledge} ${this.hobbies}`
+        console.log(info)
+    },
+}
+user.showInfo()
+user.addKnowledge("newKnowledge")
+user.addKnowledge("newKnowledge2")
+user.addKnowledge("newKnowledge3")
+user.showKnowledge()
+user.addHobby("newHobby")
+user.addHobby("newHobby2")
+user.addHobby("newHobby3")
+user.showHobby()
+user.clearKnowledge()
+user.clearHobbies()
+
 
 /*
-    Дан объект users
+    Дан объект user
     Реализовать методы:
      findById(id) возвращает элемент по id из массива users.list
      findByName(name) возвращает элемент по name из массива users.list
@@ -52,16 +112,16 @@
 */
 
 const users = {
-  list: [
-    { id: 0, name: "Vlad" },
-    { id: 1, name: "Vova" },
-    { id: 2, name: "Victor" },
-    { id: 3, name: "Kate" },
-  ],
-  findById(id) {},
-  findByName(name) {},
-  filterById(id) {},
-  filterByName(name) {},
-  getAllNames() {},
-  getAllIds() {},
+    list: [
+        { id: 0, name: "Vlad" },
+        { id: 1, name: "Vova" },
+        { id: 2, name: "Victor" },
+        { id: 3, name: "Kate" },
+    ],
+    findById(id) {},
+    findByName(name) {},
+    filterById(id) {},
+    filterByName(name) {},
+    getAllNames() {},
+    getAllIds() {},
 };
--- lesson-2/homework/scrpt.js
+++ lesson-2/homework/scrpt.js
@@ -5,11 +5,42 @@
     РЕШАТЬ ЧЕРЕЗ reduce
 */
 
+
+let array = [1, 2, 3, 1, 2, 3, 5, 4, 6, 7, 3, 2, 1, 2, 3, 5, 3, 3, 1, 2, 3, 4, 1];
+
+let compact = (array) => {
+    return array.reduce((acc, item) => {
+        if (!acc.includes(item)) {
+            acc.push(item)
+        }
+        return acc
+    }, [])
+}
+console.log(compact(array));
+
+
+// array.reduce((index, item) => {
+//     return index.includes(item) ? index : [...index, item];
+// }, []);
+// console.log();
+
 /*  
     Создать функцию с именем fromStringToObj
     Функция должна примать строку и возвращать объект
     'a.b.c.d.e.f.g.h.i' => { a: { b: { c: { d: { e: { f: { g: { h: { i: {} } } } } } } } } };
 */
+
+let fromStringToObj = (string) => {
+    const strArr = string.split('.')
+    return strArr.reduceRight((acc, item) => {
+        acc = {
+            [item]: acc
+        }
+        return acc
+    }, {})
+}
+console.log(fromStringToObj('a.b.c.d.e.f.g.h.i'))
+
 
 /*
     Реализовать CRUD для объекта todo
@@ -21,19 +52,19 @@
       deleteTodo(id) удаляет задание по id
 */
 
-const generateId = (x = 999) => Math.random() * x;
+// const generateId = (x = 999) => Math.random() * x;
 
-const testTodo = {
-  id: generateId(),
-  title: "Название задания",
-  description: null,
-  completed: false,
-};
+// const testTodo = {
+//     id: generateId(),
+//     title: "Название задания",
+//     description: null,
+//     completed: false,
+// };
 
-const todo = {
-  list: [testTodo],
-  getTodo(id) {},
-  createTodo(title, description) {},
-  updateTodo(id, newTodo) {},
-  deleteTodo(id) {},
-};
+// const todo = {
+//     list: [testTodo],
+//     getTodo(id) {},
+//     createTodo(title, description) {},
+//     updateTodo(id, newTodo) {},
+//     deleteTodo(id) {},
+// };
