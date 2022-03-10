bd9e3a7abc262e000c00bba9cc7945754cec2541
Yuriy Dubrovskiy <jr.dubrovskiy@gmail.com>
2022-03-11 00:19:10 +0300

    dubrovskiy-lesson-2: added lesson-2 homework

--- lesson-2/homework/scrpt.js
+++ lesson-2/homework/scrpt.js
@@ -1,3 +1,5 @@
+//-----------------------------------------------------------------------------------
+
 /*
     Дан массив [1,2,3,1,2,3,5,4,6,7,3,2,1,2,3,5,3,3,1,2,3,4,1]
     Создать массив из уникальных элементов
@@ -5,11 +7,58 @@
     РЕШАТЬ ЧЕРЕЗ reduce
 */
 
+const uniqueNumbers = (array) => {
+  return array.reduce((acc, curr) => {
+    if(array.indexOf(curr) === array.lastIndexOf(curr)) {
+      return [
+        ...acc,
+        curr
+      ]
+    } else {
+      return acc
+    }
+  }, [])
+}
+
+// const uniqueNumbers = (array) => {
+//   const result = []
+//   array.forEach(item => {
+//     if(array.indexOf(item) === array.lastIndexOf(item)) {
+//       result.push(item)
+//     }
+//   })
+//   return result
+// }
+
+console.log(uniqueNumbers([1,2,3,1,2,3,5,4,6,7,3,2,1,2,3,5,3,3,1,2,3,4,1]))
+
+//-----------------------------------------------------------------------------------
+
 /*  
     Создать функцию с именем fromStringToObj
     Функция должна примать строку и возвращать объект
     'a.b.c.d.e.f.g.h.i' => { a: { b: { c: { d: { e: { f: { g: { h: { i: {} } } } } } } } } };
 */
+
+// const fromStringToObj = (string) => {
+//   return string
+//     .split('.')
+//     .reduce((acc, curr) => {
+//       return {...acc}
+//     }, {curr: {},})
+// }
+
+const fromStringToObj = (string) => {
+  return string
+    .split('.')
+    .reduceRight((acc, curr) => {
+      return {[curr]: acc}
+    }, {})
+}
+
+console.log(fromStringToObj('a.b.c.d.e.f.g.h.i'))
+
+//-----------------------------------------------------------------------------------
 
 /*
     Реализовать CRUD для объекта todo
@@ -23,17 +72,33 @@
 
 const generateId = (x = 999) => Math.random() * x;
 
-const testTodo = {
-  id: generateId(),
-  title: "Название задания",
-  description: null,
-  completed: false,
-};
+// const testTodo = {
+//   id: generateId(),
+//   title: "Название задания",
+//   description: null,
+//   completed: false,
+// };
 
 const todo = {
-  list: [testTodo],
-  getTodo(id) {},
-  createTodo(title, description) {},
-  updateTodo(id, newTodo) {},
-  deleteTodo(id) {},
+  list: [],
+  getTodo(id) {
+    return this.list.find(item => item.id === id)
+  },
+  createTodo(title, description) {
+    this.list.push({
+      id: generateId(),
+      title: `${title}`,
+      description: `${description}`,
+      completed: false,
+    })
+  },
+  updateTodo(id, newTodo) {
+    const todoIndex = this.list.findIndex(item => item.id === id)
+    this.list.splice(todoIndex, 1, newTodo)
+  },
+  deleteTodo(id) {
+    this.list = this.list.filter(item => item.id !== id)
+  },
 };
+
+//-----------------------------------------------------------------------------------
