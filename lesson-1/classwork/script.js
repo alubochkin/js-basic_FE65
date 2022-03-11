b8fccebbbb344faacd2edd70b86bdb786e7eacc5
Tamara Yablonskaya <alekseevna.ta2703@gmail.com>
2022-03-11 16:14:04 +0300

    yablonskaya-lesson-1: added lesson-1 homework

--- lesson-1/classwork/script.js
+++ lesson-1/classwork/script.js
@@ -1,33 +1,71 @@
-/*
-    Создать функцию с именем duplicate
-    Функция должна принимать массив
-    Функция должна возратить удвоенный массив
-    Пример: [1,2,3] => [1,2,3,1,2,3] 
+/*Создать функцию с именем duplicate
+Функция должна принимать массив
+Функция должна возратить удвоенный массив
+Пример: [1,2,3] => [1,2,3,1,2,3] 
 */
-
+const duplicate = (arr) => [...arr, ...arr];
+console.log(duplicate([1, 2, 3]));
 /*
-    Создайте массив с 10 уникальными числами
-    * для рандомного числа используйте Math.floor(Math.random() * 100)
+Создайте массив с 10 уникальными числами
+* для рандомного числа используйте Math.floor(Math.random() * 100)
 */
+function newRandom() {
 
+    return (Math.floor(Math.random() * 100)).length < 10;
+}
+console.log(newRandom());
+console.log(newRandom());
+console.log(newRandom());
+console.log(newRandom());
+console.log(newRandom());
+console.log(newRandom());
+console.log(newRandom());
+console.log(newRandom());
+console.log(newRandom());
+console.log(newRandom());
 /*
-    Создать функцию с именем getBiggest
-    Функция должна возвращать самое большое число из переданных параметров
-    Функция должна уметь работать с разным количетвом параметров
+Создать функцию с именем getBiggest
+Функция должна возвращать самое большое число из переданных параметров
+Функция должна уметь работать с разным количетвом параметров
 */
-
+const getBiggest = (...args) => {
+    let res = Math.max(...args);
+    return res;
+}
+console.log(getBiggest(1, 2, 3, 105, 20));
 /*
-   Создать функцию с именем isAnagram
-   Написать код функции проверки анаграммы
-   isAnagram('пасечник','песчинка'); // true
-   isAnagram('xyz','xyzx'); // false
+Создать функцию с именем isAnagram
+Написать код функции проверки анаграммы
+isAnagram('пасечник','песчинка'); // true
+isAnagram('xyz','xyzx'); // false
 */
-
+const isAnagram = (left, right) => {
+    if (left.length === right.length) {
+        return true
+    } else {
+        return false
+    }
+}
+console.log(isAnagram('пасечник', 'песчинка'));
+console.log(isAnagram('xyz', 'xyzx'));
 /*
-    Создайте массив с десятью случайными элементами от 0 до 100;
-    Создать функцию с именем replacer, которая будет заменять все “0” на строку “zero”;
-    Функция должна возвращать новый массив
-    Пример: ["1zero2", 53, "2zero", 18, 22, "1zerozero", 43, 57, "5zero", 1];
-
-    * для рандомного числа используйте Math.floor(Math.random() * 100)
+Создайте массив с десятью случайными элементами от 0 до 100;
+Создать функцию с именем replacer, которая будет заменять все “0” на строку “zero”;
+Функция должна возвращать новый массив
+Пример: ["1zero2", 53, "2zero", 18, 22, "1zerozero", 43, 57, "5zero", 1];
+* для рандомного числа используйте Math.floor(Math.random() * 100)
 */
+let elementArr = [];
+const newRandom = () => {
+    while (elementArr.length < 10) {
+        let element = Math.floor(Math.random() * 100);
+        if (!elementArr.includes(element)) {
+            elementArr.push(element);
+        }
+    }
+    return elementArr;
+};
+
+const replacer = (number) => String(number).replaceAll('0', 'zero');
+console.log(replacer([20, 0, 30]));
+
--- lesson-1/homework/script.js
+++ lesson-1/homework/script.js
@@ -1,24 +1,45 @@
-/*
-    1)
-    Дана строка 'sOMe sTrING in lOWERcaSE'
-    Создать строку вида 'SoMe sTrInG In lOwErCaSe'
 
-    2)
-    Дан массив [1,2,3,1,2,3,5,4,6,7,3,2,1,2,3,5,3,3,1,2,3,4,1]
-    Создать массив из элементов которые встречаются только один раз
+// /*
+//     1)
+//     Дана строка 'sOMe sTrING in lOWERcaSE'
+//     Создать строку вида 'SoMe sTrInG In lOwErCaSe'
+let some = 'sOMe sTrING in lOWERcaSE';
+const getString = (some) => {
+  let arr = some.split("");
+  for (let i = 0; i < arr.length; i++) {
+    if (i % 2 === 0) {
+      arr[i] = arr[i].toUpperCase();
+    } else {
+      arr[i] = arr[i].toLowerCase();
+    }
+  }
+  let newSome = arr.join("");
+  return newSome;
+};
+console.log(getString(some));
+//     2)
+//     Дан массив [1,2,3,1,2,3,5,4,6,7,3,2,1,2,3,5,3,3,1,2,3,4,1]
+//     Создать массив из элементов которые встречаются только один раз
+const array = [1, 2, 3, 1, 2, 3, 5, 4, 6, 7, 3, 2, 1, 2, 3, 5, 3, 3, 1, 2, 3, 4, 1];
+const uniqArray = [...new Set(array)];
+console.log(uniqArray);
+//     3)
+//     Создать функцию с именем isPalindrome
+//     Проверить является ли строка палиндромом
+//     Пример палиндромов: 'доход', 'шалаш'
 
-    3)
-    Создать функцию с именем isPalindrome
-    Проверить является ли строка палиндромом
-    Пример палиндромов: 'доход', 'шалаш'
-
-    4)
-    Дан обьект users
-    В  окно prompt необходимо ввести имя пользователя
-    Если совпадения есть - вывести в console.log строку вида user name: USER_NAME, user email: USER_EMAIL
-
-    * Добавить возможность поиска без учёта регистра
-*/
+function isPalindrome(str) {
+  return str === str.split('').reverse().join('');
+}
+console.log(isPalindrome('доход'));
+console.log(isPalindrome('шалаш'));
+console.log(isPalindrome('дом'));
+//     4)
+//     Дан обьект users
+//     В  окно prompt необходимо ввести имя пользователя
+//     Если совпадения есть - вывести в console.log строку вида user name: USER_NAME, user email: USER_EMAIL
+//     * Добавить возможность поиска без учёта регистра
+// */
 
 const users = [
   {
@@ -40,3 +61,15 @@ const users = [
     email: "Nathan@yesenia.net",
   },
 ];
+const searchUsers = (someString) => {
+  let lowerString = someString.toLowerCase()
+  for (let key in users) {
+    if (users[key].name.toLowerCase().includes(lowerString)) {
+      console.log(`
+            user name: ${users[key].name}
+            user email: ${users[key].email}
+            `)
+    }
+  }
+}
+searchUsers(prompt('Ввести имя пользователя', 'Leanne Graham'));
