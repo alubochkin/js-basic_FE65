43810d871f50c4dbcc4d0afed3eba4fd8ce56e7d
Artur <kuchinski21@inbox.ru>
2022-03-13 17:04:40 +0300

    homework-3-kuchinski-lesson-3

--- lesson-2/homework/scrpt.js
+++ lesson-2/homework/scrpt.js
@@ -6,23 +6,18 @@
 */
 
 
-let array = [1, 2, 3, 1, 2, 3, 5, 4, 6, 7, 3, 2, 1, 2, 3, 5, 3, 3, 1, 2, 3, 4, 1];
+// let array = [1, 2, 3, 1, 2, 3, 5, 4, 6, 7, 3, 2, 1, 2, 3, 5, 3, 3, 1, 2, 3, 4, 1];
 
-let compact = (array) => {
-    return array.reduce((acc, item) => {
-        if (!acc.includes(item)) {
-            acc.push(item)
-        }
-        return acc
-    }, [])
-}
-console.log(compact(array));
+// let compact = (array) => {
+//     return array.reduce((acc, item) => {
+//         if (!acc.includes(item)) {
+//             acc.push(item)
+//         }
+//         return acc
+//     }, [])
+// }
+// console.log(compact(array));
 
-
-// array.reduce((index, item) => {
-//     return index.includes(item) ? index : [...index, item];
-// }, []);
-// console.log();
 
 /*  
     Создать функцию с именем fromStringToObj
@@ -30,21 +25,20 @@ console.log(compact(array));
     'a.b.c.d.e.f.g.h.i' => { a: { b: { c: { d: { e: { f: { g: { h: { i: {} } } } } } } } } };
 */
 
-let fromStringToObj = (string) => {
-    const strArr = string.split('.')
-    return strArr.reduceRight((acc, item) => {
-        acc = {
-            [item]: acc
-        }
-        return acc
-    }, {})
-}
-console.log(fromStringToObj('a.b.c.d.e.f.g.h.i'))
+// let fromStringToObj = (string) => {
+//     const strArr = string.split('.')
+//     return strArr.reduceRight((acc, item) => {
+//         acc = {
+//             [item]: acc
+//         }
+//         return acc
+//     }, {})
+// }
+// console.log(fromStringToObj('a.b.c.d.e.f.g.h.i'))
 
 
 /*
     Реализовать CRUD для объекта todo
-
     в объекте todo есть поле list в котором должны храниться наши задания
       getTodo(id) возвращает задание по id
       createTodo(title, description) создаёт новое задание и добавлят в массив
@@ -52,19 +46,43 @@ console.log(fromStringToObj('a.b.c.d.e.f.g.h.i'))
       deleteTodo(id) удаляет задание по id
 */
 
-// const generateId = (x = 999) => Math.random() * x;
+const generateId = (x = 999) => Math.random() * x;
+const testTodo = {
+    id: 1234,
+    title: "Название задания",
+    description: null,
+    completed: false,
+};
 
-// const testTodo = {
-//     id: generateId(),
-//     title: "Название задания",
-//     description: null,
-//     completed: false,
-// };
-
-// const todo = {
-//     list: [testTodo],
-//     getTodo(id) {},
-//     createTodo(title, description) {},
-//     updateTodo(id, newTodo) {},
-//     deleteTodo(id) {},
-// };
+const todo = {
+    list: [testTodo],
+    getTodo(id) {
+        const getId = this.list.find(person => person.id === id)
+        return getId
+    },
+    createTodo(title, description) {
+        const newElem = {
+            id: generateId(),
+            title: `${title}`,
+            description: `${description}`,
+            completed: false,
+        }
+        this.list.push(newElem)
+        return newElem
+    },
+    updateTodo(id, newTodo) {
+        this.list = this.list.map(person => {
+            if (person.id === id) {
+                return newTodo
+            }
+        })
+        return this.list
+    },
+    deleteTodo(id) {
+        const deleteId = this.list.filter(person => person.id !== id)
+        return deleteId
+    },
+};
+console.log(todo.getTodo(1234));
+console.log(todo.createTodo('newTodo', 'asdf'));
+console.log(todo.updateTodo(1234, { id: 826 }));
--- lesson-2/homework2/script.js
+++ lesson-2/homework2/script.js
@@ -16,6 +16,7 @@
             }
         }
     }
+    
 
     get(obj, 'a.b.c') // 1
     get(obj, 'a.b') // { c: '1' }
@@ -23,22 +24,65 @@
     get(obj, 'x.b.z') // undefined
 */
 
+const obj = {
+    a: {
+        b: {
+            c: '1'
+        }
+    },
+    x: {
+        y: {
+            z: '2'
+        }
+    }
+}
+const path = 'a.b.c'
+const path2 = 'x.y.z'
+const get = (obj, path) => {
+    const result = path.split('.')
+    return result.reduce((acc, cur) => {
+        acc = acc[cur]
+        return acc
+    }, obj)
+}
+console.log(get(obj, path2));
+console.log(get(obj, path));
+
+
+
+
 /*
     Функция, которая будет переворачивать число
 
     reverseInt(15) === 51
     reverseInt(500) === 5
 */
+const reverseFunc = (args) => {
+    const reversed = +String(args).split('').reverse().join('')
+    return reversed
+}
+console.log(reverseFunc(506000))
+    /* 
+        Напишите Функции, которая возвращает новый массив из общих элементов двух массивов
 
-/* 
-    Напишите Функции, которая возвращает новый массив из общих элементов двух массивов
+        const a = [1,2,3,4,5];
+        const b = [1,3,6,3,1,4,7];
 
-    const a = [1,2,3,4,5];
-    const b = [1,3,6,3,1,4,7];
+        getCommonElements(a, b); //[1,3,4]
+    */
+const a = [1, 2, 3, 4, 5];
+const b = [1, 3, 6, 3, 1, 4, 7]
 
-    getCommonElements(a, b); //[1,3,4]
-*/
-
+let compact = (a, b) => {
+    let c = []
+    for (let i = 0; i <= a.length; i++) {
+        if (b.includes(a[i])) {
+            c.push(a[i])
+        }
+    }
+    return c
+}
+console.log(compact(a, b));
 /* 
     Напишите функцию которая работает таким образом
 
@@ -46,10 +90,25 @@
     arr.forEach(cb(10)); // 11 12 13 14 15
 
 */
+const arr = [1, 2, 3, 4, 5];
 
+const cb = (num) => (item) => {
+    console.log(num + item);
+}
+
+const number = cb(10)
+arr.forEach(cb(10)); // 11 12 13 14 15
 /*
     Функция, которая будет работать так
 
     add(1,2)  //3
     add(2)(4) //6
 */
+
+const sum = (a, b) => {
+    if (!b) {
+        return (z) => a + z
+    }
+    return a + b
+}
+console.log(sum(2)(7));
