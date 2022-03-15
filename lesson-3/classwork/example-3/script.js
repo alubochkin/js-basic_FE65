9c7e8ec17e83a697d45b10a2036c884b7dc0b6aa
Yuriy Dubrovskiy <jr.dubrovskiy@gmail.com>
2022-03-15 19:44:05 +0300

    dubrovskiy-lesson-3: added files

---
+++ lesson-3/classwork/example-3/script.js
@@ -0,0 +1,28 @@
+// console.log(document.body);
+
+// setTimeout(() => {
+//   document.body.innerHTML = "<h1>Изменили document.body из script.js</h1>";
+// }, 2000);
+
+// ------------------------------------------
+
+// const todoList = document.getElementById("todo-list");
+
+// setTimeout(() => {
+//   todoList.outerHTML = "<h1>Изменили document.body из script.js</h1>";
+// }, 2000);
+
+// ------------------------------------------
+
+// const todoList = document.getElementById("todo-list");
+
+// console.log(todoList.outerHTML);
+// console.log(todoList.textContent);
+
+// const text = "<h1>Hello world</h1>";
+
+// const elem1 = document.getElementById("innerHTML");
+// const elem2 = document.getElementById("textContent");
+
+// elem1.innerHTML = text
+// elem2.textContent = text
