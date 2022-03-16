14810daffc55ccf229c9439486a30b120eae81f7
masha <mstroeva976@gmail.com>
2022-03-16 23:23:03 +0300

    merge master in stroeva

--- lesson-3/classwork/script.js
+++ lesson-3/classwork/script.js
@@ -15,6 +15,32 @@
     Добавьте элемент ul перед h3
 */
 
+const h3 = document.createElement('h3')
+// h3.className = 'title'
+h3.classList.add('title')
+h3.textContent = 'h3'
+document.body.append(h3)
+
+
+const p = document.createElement('p')
+// h3.className = 'title'
+p.classList.add('title')
+p.textContent = 'p'
+document.body.append(p)
+
+const ul = document.createElement("ul")
+ul.classList.add("list")
+ul.textContent = "list"
+document.body.prepend(ul)
+
+const liOne = document.createElement("li")
+const liTwo = document.createElement("li")
+const liThree = document.createElement("li");
+// liOne.textContent = 'ffnkd'
+[liOne, liTwo, liThree].forEach(item => item.textContent = "njbbhb")
+ul.append(liOne, liTwo, liThree)
+
+
 /*
     Создать кнопку
     При каждом нажатии кнопки она должна менять цвет
@@ -29,7 +55,7 @@ const colors = ["#2656a3", "#611b94", "#9c1c7e", "#3f8a17", "#a31c1c"];
 
 /*
     Создать параграф
-    Класть в него информацию о положении курсора мыши Y:X
+    Класть в него информацию о положении кур`сора мыши Y:X
 */
 
 /*
