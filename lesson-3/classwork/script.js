24bfbd630d8c9f8fb1215f26e713a259fff57585
Yuriy Dubrovskiy <jr.dubrovskiy@gmail.com>
2022-03-17 00:48:17 +0300

    dubrovskiy-lesson-3: added classwork

--- lesson-3/classwork/script.js
+++ lesson-3/classwork/script.js
@@ -15,11 +15,49 @@
     Добавьте элемент ul перед h3
 */
 
+//------------------------------------------------------
+
+const h3 = document.createElement('h3')
+h3.classList.add('sss')
+h3.textContent = 'Hello h3'
+// h3.className = 'sss'
+document.body.append(h3)
+
+//------------------------------------------------------
+
+const p = document.createElement('p')
+p.classList.add('xxx')
+p.textContent = 'Hello p'
+document.body.append(p)
+
+//------------------------------------------------------
+
+const ul = document.createElement("ul");
+const li1 = document.createElement("li");
+const li2 = document.createElement("li");
+const li3 = document.createElement("li");
+const li4 = document.createElement("li");
+const li5 = document.createElement("li");
+
+[li1, li2, li3, li4, li5].forEach((item, index) => {
+    item.textContent = `li № ${index + 1}`
+});
+ul.className = 'ul_class'
+
+ul.append(li1, li2, li3, li4, li5)
+h3.before(ul)
+
+//------------------------------------------------------
+
 /*
     Создать кнопку
     При каждом нажатии кнопки она должна менять цвет
     цвет брать из массива colors
 */
+
+const btnRandomColor = document.createElement("button")
+btnRandomColor.textContent = 'random color'
+document.body.append(btnRandomColor)
 
 function getRandom(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
@@ -27,10 +65,34 @@ function getRandom(min, max) {
 
 const colors = ["#2656a3", "#611b94", "#9c1c7e", "#3f8a17", "#a31c1c"];
 
+btnRandomColor.style.background = colors[getRandom(0,5)]
+
+btnRandomColor.addEventListener('click', () => {
+    btnRandomColor.style.background = colors[getRandom(0,5)]
+})
+
+//------------------------------------------------------
+
 /*
     Создать параграф
     Класть в него информацию о положении курсора мыши Y:X
 */
+
+const p1 = document.createElement("p")
+document.body.append(p1)
+p1.textContent = `
+x:
+y:
+`
+
+document.addEventListener('mouseover', (event) => {
+    p1.textContent = `
+    x: ${event.clientX}
+    y: ${event.clientY}
+    `
+})
+
+//------------------------------------------------------
 
 /*
     Создать параграф с рандомным текстом
@@ -39,3 +101,29 @@ const colors = ["#2656a3", "#611b94", "#9c1c7e", "#3f8a17", "#a31c1c"];
 
     Нужно чтобы context menu по умолчанию не открывался
 */
+
+const p2 = document.createElement("p")
+p2.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt labore numquam, pariatur sint qui debitis a itaque voluptate delectus quibusdam hic non saepe laudantium excepturi unde autem accusantium perspiciatis deleniti!"
+p2.className = 'p2'
+let fSize = 16
+p2.style.fontSize = '16px'
+document.body.append(p2)
+console.log(p2.style)
+
+
+// function test() {return false}
+// p2.oncontextmenu = test;
+
+
+p2.addEventListener('click', () => {
+    fSize += 2
+    p2.style.fontSize = `${fSize}px`
+})
+
+p2.addEventListener('contextmenu', (e) => {
+    e.preventDefault()
+    fSize -= 2
+    p2.style.fontSize = `${fSize}px`
+})
+
+//------------------------------------------------------
