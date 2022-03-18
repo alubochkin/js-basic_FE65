f3d5307a7ce066b9aa55e5143024922127b8658d
Artur <kuchinski21@inbox.ru>
2022-03-18 19:36:49 +0300

    added tasks

--- lesson-3/homework/script.js
+++ lesson-3/homework/script.js
@@ -4,59 +4,94 @@
     1. через ивент на каждую внопку
     2. через делегирование событий
 */
+let buttonPlus = document.querySelector(".plus")
+let counterValue = document.querySelector('.count')
+let buttonMinus = document.querySelector('.minus')
+let buttonReset = document.querySelector('.reset')
+let buttons = document.getElementById('actions')
 
-/*
-    Создать функцию createElement
-    Она должна принимать первым параметром имя тега,  вторым объект с свойствами и методами для для элемента (textContent, innerHtml, className, ...и тд)
+// buttonPlus.onclick = function() {
+//     counterValue.innerHTML = +counterValue.innerHTML + 1
+// }
 
-    Причина
-    нам нужно избавиться от длинной записи типа
+// buttonMinus.onclick = function() {
+//     counterValue.innerHTML = +counterValue.innerHTML - 1
+// }
 
-    const h1 = document.createElement('h1')
-    h1.className = 'title'
-    h1.textContent = 'Main title'
-    h1.style.fontSize = '18px'
+// buttonReset.onclick = function() {
+//     counterValue.innerHTML = 0
+// }
 
-    нужно сделать так
+buttons.addEventListener('click', (event) => {
+        if (event.target.classList.value === "plus") {
+            counterValue.innerHTML = +counterValue.innerHTML + 1
+        } else if (event.target.classList.value === 'minus') {
+            counterValue.innerHTML = +counterValue.innerHTML - 1
+        } else if (event.target.classList.value === 'reset') {
+            counterValue.innerHTML = 0
+        }
+    })
+    /*
+        Создать функцию createElement
+        Она должна принимать первым параметром имя тега,  вторым объект с свойствами и методами для для элемента (textContent, innerHtml, className, ...и тд)
 
-    const h1 = createElement("h1", {
-        className: "title",
-        textContent: "Main title",
-        style: "font-size: 48px, text-align: center",
-    });
+        Причина
+        нам нужно избавиться от длинной записи типа
 
-*/
+        const h1 = document.createElement('h1')
+        h1.className = 'title'
+        h1.textContent = 'Main title'
+        h1.style.fontSize = '18px'
 
-const createElement = (tagName, options = {}) => {
-  // your code
+        нужно сделать так
+
+        const h1 = createElement("h1", {
+            className: "title",
+            textContent: "Main title",
+            style: "font-size: 48px, text-align: center",
+        });
+
+    */
+
+let someObject = {
+    className: "title",
+    textContent: "Main title",
+    style: "font-size: 48px, text-align: center",
+}
+
+const createElement = (tagName, object = {}) => {
+    const tag = document.createElement(tagName)
+    if (object.hasOwnProperty("className")) {
+        tag.setAttribute('class', object.className)
+    }
+    if (object.hasOwnProperty('textContent')) {
+        tag.setAttribute('text', object.textContent)
+    }
+    if (object.hasOwnProperty('style')) {
+        tag.setAttribute('style', object.style)
+    }
+    return tag
 };
-
-const h1 = document.createElement("h1");
-h1.className = "title";
-h1.textContent = "Main title";
-h1.style.fontSize = "48px";
-h1.style.textAlign = "center";
-
-document.body.append(h1);
+console.log(createElement('h1', someObject))
 
 /*
     Реализовать модель туду листа
 */
 
 const todoItem = {
-  id: 1,
-  title: "title",
-  description: "description",
-  completed: false,
+    id: 1,
+    title: "title",
+    description: "description",
+    completed: false,
 };
 
 const todo = {
-  list: [todoItem],
-  getById(id) {},
-  createTodo(newTodo) {},
-  updateTodo(id, newTodo) {},
-  deleteTodo(id) {},
-  sortByTitle() {},
-  sortByDescription() {},
-  searchByTitle(title) {},
+    list: [todoItem],
+    getById(id) {},
+    createTodo(newTodo) {},
+    updateTodo(id, newTodo) {},
+    deleteTodo(id) {},
+    sortByTitle() {},
+    sortByDescription() {},
+    searchByTitle(title) {},
 };
