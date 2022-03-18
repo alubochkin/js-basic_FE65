e35666bc19f0064a6101d6adce1777c71332e568
Valiantsin Dashkevich <v6087573@gmail.com>
2022-03-18 19:10:33 +0300

    added lesson 3

--- lesson-3/homework/script.js
+++ lesson-3/homework/script.js
@@ -4,7 +4,26 @@
     1. через ивент на каждую внопку
     2. через делегирование событий
 */
+const actions = document.getElementById("actions");
+const minus = document.querySelector("minus");
+const reset = document.querySelector("reset");
+const plus = document.querySelector("plus");
+const count = document.querySelector("p.count");
 
+
+actions.addEventListener("click", (event) => {
+  if (event.target.classList.contains("minus")) {
+    count.textContent = +count.textContent - 1
+  }
+
+  if (event.target.classList.contains("reset")) {
+    count.textContent = '0'
+  }
+
+  if (event.target.classList.contains("plus")) {
+    count.textContent = +count.textContent + 1
+  }
+});
 /*
     Создать функцию createElement
     Она должна принимать первым параметром имя тега,  вторым объект с свойствами и методами для для элемента (textContent, innerHtml, className, ...и тд)
@@ -28,7 +47,9 @@
 */
 
 const createElement = (tagName, options = {}) => {
-  // your code
+    const el = document.createElement(tagName)
+    Object.assign(el, options)
+    return el
 };
 
 const h1 = document.createElement("h1");
@@ -39,6 +60,14 @@ h1.style.textAlign = "center";
 
 document.body.append(h1);
 
+const title = createElement("h1", {
+  className: "title",
+  textContent: "Main title",
+  style: "font-size: 48px, text-align: center",
+});
+
+
+console.log(title)
 /*
     Реализовать модель туду листа
 */
