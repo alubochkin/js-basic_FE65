6a6f6de38f932bdda8758f9b565b7cb1553e4528
Ivan Kiselkov <ivan131100@icloud.com>
2022-03-18 13:48:26 +0300

    added lesson-3 classwork

--- lesson-3/classwork/script.js
+++ lesson-3/classwork/script.js
@@ -15,6 +15,25 @@
     Добавьте элемент ul перед h3
 */
 
+const h3 = document.createElement("h3");
+h3.textContent = "h333333";
+h3.classList.add("h3");
+document.body.append(h3);
+
+const text = document.createElement("p");
+text.textContent = "Lorem ipsum dolor";
+text.classList.add("text");
+document.body.append(text);
+
+const list = document.createElement("ul");
+list.innerHTML = `
+    <li>liii</li>
+    <li>liii</li>
+    <li>liii</li>
+`;
+list.classList.add("list");
+document.body.prepend(list);
+
 /*
     Создать кнопку
     При каждом нажатии кнопки она должна менять цвет
@@ -27,10 +46,26 @@ function getRandom(min, max) {
 
 const colors = ["#2656a3", "#611b94", "#9c1c7e", "#3f8a17", "#a31c1c"];
 
+const btn = document.createElement("button");
+btn.textContent = "Click";
+btn.classList.add("btn");
+document.body.append(btn);
+
+btn.onclick = () => {
+  btn.style.backgroundColor = colors[getRandom(0, colors.length)];
+};
+
 /*
     Создать параграф
     Класть в него информацию о положении курсора мыши Y:X
 */
+
+const p = document.createElement("p");
+p.classList.add("p");
+document.body.append(p);
+document.body.onmousemove = (event) => {
+  p.textContent = `${event.pageX}:${event.pageY}`;
+};
 
 /*
     Создать параграф с рандомным текстом
@@ -39,3 +74,16 @@ const colors = ["#2656a3", "#611b94", "#9c1c7e", "#3f8a17", "#a31c1c"];
 
     Нужно чтобы context menu по умолчанию не открывался
 */
+
+const paragraph = document.createElement("p");
+paragraph.classList.add("p");
+document.body.append(paragraph);
+paragraph.textContent = "Random Text";
+paragraph.style.fontSize = "18px";
+let fz = parseInt(paragraph.style.fontSize);
+
+paragraph.onmousedown = (event) => {
+  event.which === 1
+    ? (paragraph.style.fontSize = `${fz + 2}px`)
+    : (paragraph.style.fontSize = `${fz - 2}px`);
+};
