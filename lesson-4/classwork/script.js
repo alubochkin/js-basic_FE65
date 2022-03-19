6993e501c0d6922983313451fdf00b4b8f7dca8f
Yuriy Dubrovskiy <jr.dubrovskiy@gmail.com>
2022-03-19 03:05:16 +0300

    dubrovskiy-lesson-4: add classwork

--- lesson-4/classwork/script.js
+++ lesson-4/classwork/script.js
@@ -10,6 +10,36 @@
         const array1 = [1,1,1,2,3,3,3]\
         console.log(array1.uniq()); // [1,2,3]
 */
+const array = [1,2,3,4,5,6,7,8,9]
+const array1 = [1,1,1,2,3,3,3]
+
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+Array.prototype.sum = function() {
+  return this.reduce((acc, cur) => acc + cur)
+}
+
+Array.prototype.multiply = function() {
+  return this.reduce((acc, cur) => acc * cur)
+}
+
+Array.prototype.uniq = function() {
+  const res = []
+  this.forEach(item => res.includes(item) ? item : res.push(item))
+  return res
+}
+
+Array.prototype.randomElem = function() {
+  return this[getRandom(0, this.length)]
+}
+
+console.log(array)
+console.log(array.sum())
+console.log(array.multiply())
+console.log(array1.uniq())
+console.log(array.randomElem())
 
 /*
     Добавить метод reverse в прототип объекта String
@@ -19,10 +49,26 @@
         console.log(string.reverse()); gnirts emos
 */
 
+String.prototype.reverse = function() {
+  return this.split('').reverse().join('')
+}
+
+const string = 'some string'
+console.log(string.reverse())
+
 /*
     Создать input
     При перезагрузке страницы в инпуте должно быть последнее введённое значение
 */
+
+const input = document.createElement('input')
+document.body.append(input)
+input.value = localStorage.getItem('inputValue')
+
+input.addEventListener('input', (event) => {
+  console.log(event.target.value)
+  localStorage.setItem('inputValue', event.target.value)
+})
 
 /*
     lesson-3/homework Реализовать модель туду листа
