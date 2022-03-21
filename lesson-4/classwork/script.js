626022b554a854c636c31fe9c7d4806bef0b7a30
masha <mstroeva976@gmail.com>
2022-03-21 21:52:12 +0300

    stroeva_lesson-4:added

--- lesson-4/classwork/script.js
+++ lesson-4/classwork/script.js
@@ -10,7 +10,37 @@
         const array1 = [1,1,1,2,3,3,3]\
         console.log(array1.uniq()); // [1,2,3]
 */
+const array = [1,2,3,4,5,6,7,8,9]
 
+Array.prototype.sum = function () {
+  // return this.reduce((acc, cur) => acc + cur, 0);
+  let res = 0;
+  for (let i = 0; i < this.length; i++) {
+    res += this[i]
+  }
+  return res
+}
+Array.prototype.multiply = function () {
+  return this.reduce((acc, cur) => acc * cur, 1)
+}
+Array.prototype.randomElem = function() {
+  const max = this.length
+  const random = Math.floor(Math.random() * (max));
+  return random
+}
+
+console.log(array)
+console.log(array.sum()); 
+console.log(array.multiply()); 
+console.log(array.randomElem()); 
+
+const array1 = [1,1,1,2,3,3,3]
+Array.prototype.uniq = function () {
+  const res = [];
+  this.forEach((item) => res.includes(item) ? item : res.push(item))
+  return res
+}
+console.log(array1.uniq()); 
 /*
     Добавить метод reverse в прототип объекта String
 
@@ -31,12 +61,12 @@
     ** реализовать функцию конструктор которая будет возвращаться объект todo
 */
 
-const todoItem = {
-  id: 1,
-  title: "title",
-  description: "description",
-  completed: false,
-};
+// const todoItem = {
+//   id: 1,
+//   title: "title",
+//   description: "description",
+//   completed: false,
+// };
 
 const todo = {
   list: [todoItem],
