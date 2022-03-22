6f6f311fd7770e8d6fa6e7235ff71d4f6fc0536d
Yuriy Dubrovskiy <jr.dubrovskiy@gmail.com>
2022-03-22 13:13:50 +0300

    dubrovskiy-lesson-4: update script.js

--- lesson-4/classwork/script.js
+++ lesson-4/classwork/script.js
@@ -77,20 +77,42 @@ input.addEventListener('input', (event) => {
     ** реализовать функцию конструктор которая будет возвращаться объект todo
 */
 
-const todoItem = {
-  id: 1,
-  title: "title",
-  description: "description",
-  completed: false,
-};
-
 const todo = {
-  list: [todoItem],
-  getById(id) {},
-  createTodo(newTodo) {},
-  updateTodo(id, newTodo) {},
-  deleteTodo(id) {},
-  sortByTitle() {},
-  sortByDescription() {},
-  searchByTitle(title) {},
+  list: localStorage.getItem('list') ? JSON.parse(localStorage.getItem('list')) : [],
+
+  updateLocal() {
+    localStorage.setItem('list', JSON.stringify(this.list))
+  },
+  getById(id) {
+      return this.list.find(item => item.id === id)
+  },
+  createTodo(newTodo) {
+      this.list = [...this.list, {
+          id: Math.random(),
+          ...newTodo,
+          completed: false
+      }]
+      this.updateLocal()
+  },
+  updateTodo(id, newTodo) {
+      this.list = this.list.map(item => item.id !== id ? item : newTodo)
+  },
+  deleteTodo(id) {
+      this.list = this.list.filter(item => item.id !== id)
+      this.updateLocal()
+  },
+  changeStatus(id) {
+      const indexEl = this.list.findIndex(item => item.id === id)
+      this.list[indexEl].status = !this.list[indexEl].status
+      this.updateLocal()
+  },
+  sortByTitle() {
+      this.list = this.list.sort((a,b) => a.title > b.title ? 1 : -1)
+  },
+  sortByDescription() {
+      this.list = this.list.sort((a,b) => a.description > b.description ? 1 : -1)
+  },
+  searchByTitle(title) {
+      return this.list.filter(item => item.title.includes(title))
+  },
 };
