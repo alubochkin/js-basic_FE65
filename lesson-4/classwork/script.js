2e90499e25da4e349db86f305bc8ab18f2f5c934
Artur <kuchinski21@inbox.ru>
2022-03-22 18:56:50 +0300

    hw4

--- lesson-4/classwork/script.js
+++ lesson-4/classwork/script.js
@@ -11,36 +11,36 @@
         console.log(array1.uniq()); // [1,2,3]
 */
 
-Array.prototype.sum = function() {
-    let s = 0;
-    for (let i = 0; i < this.length; i++) {
-        s += this[i]
-    }
-    return s
-}
+// Array.prototype.sum = function() {
+//     let s = 0;
+//     for (let i = 0; i < this.length; i++) {
+//         s += this[i]
+//     }
+//     return s
+// }
 
-Array.prototype.multiply = function() {
-    let s = 1;
-    for (let i = 0; i < this.length; i++) {
-        s *= this[i]
-    }
-    return s
-}
+// Array.prototype.multiply = function() {
+//     let s = 1;
+//     for (let i = 0; i < this.length; i++) {
+//         s *= this[i]
+//     }
+//     return s
+// }
 
-Array.prototype.uniq = function() {
-    let res = this.reduce((total, cur) => ({...total, [cur]: cur }), {})
-    return Object.values(res);
-}
+// Array.prototype.uniq = function() {
+//     let res = this.reduce((total, cur) => ({...total, [cur]: cur }), {})
+//     return Object.values(res);
+// }
 
-Array.prototype.random = function() {
-    return this[Math.floor(Math.random() * this.length)];
-}
+// Array.prototype.random = function() {
+//     return this[Math.floor(Math.random() * this.length)];
+// }
 
-const array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-console.log(array.sum())
-console.log(array.multiply())
-console.log(array.uniq());
-console.log(array.random());
+// const array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// console.log(array.sum())
+// console.log(array.multiply())
+// console.log(array.uniq());
+// console.log(array.random());
 /*
     Добавить метод reverse в прототип объекта String
 
@@ -48,23 +48,23 @@ console.log(array.random());
         const string = 'some string'
         console.log(string.reverse()); gnirts emos
 */
-const string = 'some string'
-String.prototype.reverse = function() {
-    return this.split('').reverse().join('')
-}
-console.log(string.reverse());
-/*
-    Создать input
-    При перезагрузке страницы в инпуте должно быть последнее введённое значение
-*/
-const body = document.body
-const input = document.createElement('input')
-input.value = localStorage.getItem('value') || '';
-body.prepend(input)
+// const string = 'some string'
+// String.prototype.reverse = function() {
+//     return this.split('').reverse().join('')
+// }
+// console.log(string.reverse());
+// /*
+//     Создать input
+//     При перезагрузке страницы в инпуте должно быть последнее введённое значение
+// */
+// const body = document.body
+// const input = document.createElement('input')
+// input.value = localStorage.getItem('value') || '';
+// body.prepend(input)
 
-input.addEventListener('input', (event) => {
-    localStorage.setItem('value', (event.target.value))
-})
+// input.addEventListener('input', (event) => {
+//     localStorage.setItem('value', (event.target.value))
+// })
 
 
 /*
@@ -73,6 +73,14 @@ input.addEventListener('input', (event) => {
     реллизовать сохранение todo.list в localStorage
     ** реализовать функцию конструктор которая будет возвращаться объект todo
 */
+const form = document.querySelector("#form");
+const todos = document.querySelector("#todo");
+const inputText = document.querySelector("#text");
+const inputDescription = document.querySelector("#description");
+const btnSubmit = document.querySelector("#btnSubmit");
+const btnReset = document.querySelector("#btnReset");
+const btnsearchtitle = document.querySelector("#btnsearchtitle");
+const btnsearcdescription = document.querySelector("#btnsearcdescription");
 
 const generateId = (x = 999) => Math.random() * x;
 const todoItem = {
@@ -89,38 +97,34 @@ const todo = {
         return getId
     },
     createTodo(newTodo) {
-        const newTodo = {
+        this.list.push = {
             id: generateId(),
-            title: `${title}`,
+            title: `${newTodo}`,
             description: `${description}`,
             completed: false,
         }
-        this.list.push(newTodo)
-        return newTodo
     },
     updateTodo(id, newTodo) {
         this.list = this.list.map(todo => {
             if (todo === id) {
-                return {...todo, ...newtodo }
+                return {...todo, ...newTodo }
             }
             return todo
         })
         return this.list
     },
     deleteTodo(id) {
-        const deleteId = this.list.filter(person => person.id !== id)
-        return deleteId
+        this.list = this.list.filter(person => person.id !== id)
+
     },
     sortByTitle() {
         const sortTitle = this.list.sort((a, b) => (a.title > b.title ? -1 : 1))
-        return sortTitle
     },
     sortByDescription() {
         const sortDescription = this.list.sort((a, b) => (a.description > b.description ? -1 : 1))
-        return sortDescription
     },
     searchByTitle(title) {
-        const searchTitle = this.list.find(person => person.id === id)
+        const searchTitle = this.list.filter(element => element.title.includes(title))
         return searchTitle
     },
     saveLocalStorage() {
