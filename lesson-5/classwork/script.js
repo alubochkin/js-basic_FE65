2780b258545829a4e49c54fcf8a03f29d0b59309
vladislav-tarasov-tms <vladtarasov306@gmail.com>
2022-03-22 21:37:37 +0300

    added todo class

--- lesson-5/classwork/script.js
+++ lesson-5/classwork/script.js
@@ -10,3 +10,43 @@
     console.log(atm.getMoney(11500)); // 2X5000 1X1000 1X500
     console.log(atm.getMoney(4500)); // 4X1000 1X500 
 */
+
+class Todo {
+  constructor(initialList) {
+    this.list = initialList;
+  }
+
+  get list() {
+    return this._list;
+  }
+
+  set list(value) {
+    this._list = value;
+    localStorage.setItem("todo-list", JSON.stringify(value));
+  }
+
+  createTodo(newTodo) {
+    // this.list = this.list.concat({ id: Math.random(), ...newTodo });
+    this.list = [...this.list, { id: Math.random(), ...newTodo }];
+  }
+
+  updateTodo(id, newTodo) {
+    this.list = this.list.map((todo) =>
+      todo.id === id ? { ...todo, ...newTodo } : todo
+    );
+  }
+
+  deleteTodo(id) {
+    this.list = this.list.filter((todo) => todo.id !== id);
+  }
+
+  searchByTitle(title) {
+    return this.list.filter((todo) =>
+      todo.title.toLowerCase().includes(title.toLowerCase())
+    );
+  }
+}
+
+const todoList = new Todo([]);
+todoList.createTodo({ title: "title" });
+console.log(todoList.list);
