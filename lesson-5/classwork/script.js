7b0dfbfbb941a5653c37553814d33103c9741096
Artur <kuchinski21@inbox.ru>
2022-03-22 21:35:07 +0300

    added cw5

--- lesson-5/classwork/script.js
+++ lesson-5/classwork/script.js
@@ -10,3 +10,48 @@
     console.log(atm.getMoney(11500)); // 2X5000 1X1000 1X500
     console.log(atm.getMoney(4500)); // 4X1000 1X500 
 */
+
+
+class Todo {
+    constructor(initialList) {
+        this.list = initialList;
+    }
+    get list() {
+        return this._list
+    }
+
+    set list(value) {
+        this._list = value;
+        localStorage.setItem('todo-list', JSON.stringify(value))
+    }
+
+    getById(id) {
+        return getId = this.list.find(person => person.id === id)
+    }
+
+    createTodo(newTodo) {
+        this.list = [...this.list, { id: Math.random(), ...newTodo }]
+    }
+    deleteTodo(id) {
+        this.list = this.list.filter(person => person.id !== id)
+    }
+    updateTodo(id, newTodo) {
+        this.list = this.list.map(todo => {
+            if (todo === id) {
+                return {...todo, ...newTodo }
+            }
+            return todo
+        })
+    }
+    searchByTitle(title) {
+        return this.list.filter(todo =>
+            todo.title.toLowerCase().includes(title.toLowerCase()))
+    }
+
+    sortByDescription() {
+        this.list.sort((a, b) => (a.description > b.description ? -1 : 1))
+    }
+
+}
+const todoList = new Todo([])
+todoList.createTodo({ title: 'dasfsd' });
