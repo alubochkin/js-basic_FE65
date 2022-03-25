25034866b0e4658346ee9655197564fbf0429cdb
Yuriy Dubrovskiy <jr.dubrovskiy@gmail.com>
2022-03-25 20:59:02 +0300

    dubrovskiy-lesson-5: added classwork

--- lesson-5/classwork/script.js
+++ lesson-5/classwork/script.js
@@ -10,3 +10,56 @@
     console.log(atm.getMoney(11500)); // 2X5000 1X1000 1X500
     console.log(atm.getMoney(4500)); // 4X1000 1X500 
 */
+
+class ATM {
+
+}
+
+class Todo {
+    constructor(initialList) {
+        this.list = initialList
+    }
+
+    get list() {
+        return this._list
+    }
+
+    set list(value) {
+        this._list = value
+        localStorage.setItem('todo-list', JSON.stringify(value))
+    }
+
+    getById(id) {
+        return this.list.find(item => item.id === id)
+    }
+
+    createTodo(newTodo) {
+        this.list = [...this.list, {
+            id: Math.random(),
+            ...newTodo,
+            completed: false
+        }]
+    }
+
+    updateTodo(id, newTodo) {
+        this.list = this.list.map(item => item.id !== id ? item : {...item, ...newTodo})
+    }
+
+    deleteTodo(id) {
+        this.list = this.list.filter(item => item.id !== id)
+    }
+
+    sortByTitle() {
+        this.list.sort((a,b) => a.title > b.title ? 1 : -1)
+    }
+
+    sortByDescription() {
+        this.list.sort((a,b) => a.description > b.description ? 1 : -1)
+    }
+
+    searchByTitle(title) {
+        return this.list.filter(item => item.title.toLoweCase().includes(title.toLoweCase()))
+    }
+}
+
+const todo = new Todo([])
