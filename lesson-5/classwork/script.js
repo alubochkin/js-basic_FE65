d8808ac520f6c8af63299bebb81654fffe757a1f
vladislav-tarasov-tms <vladtarasov306@gmail.com>
2022-03-28 19:35:13 +0300

    added lesson-6 & lesson-7

--- lesson-5/classwork/script.js
+++ lesson-5/classwork/script.js
@@ -50,3 +50,70 @@ class Todo {
 const todoList = new Todo([]);
 todoList.createTodo({ title: "title" });
 console.log(todoList.list);
+
+/*
+  1. Форму *Валидация
+  2. Вывод
+  3. Фильтрация. Сортировка
+  4. Туду экшены
+*/
+
+const input = document.querySelector("input");
+const description = document.querySelector("description");
+const span = document.querySelector("span");
+const button = document.querySelector("button");
+const ul = document.querySelector("ul");
+
+const validation = (input, description) => {
+  let error = {};
+
+  if (input.length === 0) {
+    error.input = "required";
+  }
+
+  if (description.length > 100) {
+    error.description = "max length 100";
+  }
+
+  return Object.keys(error).length ? error : null;
+};
+
+button.onclick = (e) => {
+  e.preventDefault();
+  const err = validation(input.value, description.value);
+
+  if (err?.input) {
+    input.classList.add("error");
+  }
+
+  if (err?.description) {
+    description.classList.add("error");
+  }
+
+  if (!err) {
+    todo.createTodo(input.value, description.value);
+    span.hidden = true;
+    renderTodoList(todo.list);
+  }
+};
+
+const renderTodoList = (todoList) => {
+  ul.innerHTML = "";
+  todoList.forEach((todo) => {
+    const li = document.createElement("li");
+
+    const h6 = document.createElement("h6");
+    h6.textContent = todo.title;
+    li.append(h6);
+
+    if (todo.description) {
+      const p = document.createElement("p");
+      p.textContent = todo.description;
+      li.append(p);
+    }
+
+    ul.append(li);
+  });
+};
+
+renderTodoList(JSON.parse(localStorage.getItem("todo")) ?? []); // new Todo(JSON.parse(localStorage.getItem("todo")) ?? []) - renderTodoList(todo.list)
