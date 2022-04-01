1a973e030bd4531c8616d9b66308190764450a8c
Artur <kuchinski21@inbox.ru>
2022-04-01 19:01:35 +0300

    added cw-7

--- lesson-7/classwork/script.js
+++ lesson-7/classwork/script.js
@@ -2,3 +2,57 @@
 // 1. пока выполняется запрос должен отображаться лоадер (renderLoading)
 // 2. при удачном выполнении запроса должен выводиться список юзеров (renderUsers)
 // 3. запрос не отработал как надо, показываем ошибку (renderError)
+
+const load = document.getElementById('wrapper')
+let current = 5;
+
+const renderLoading = () => {
+    load.innerHTML = '<p class="loader">Loading...</p>'
+}
+const renderEror = () => {
+    load.innerHTML = '<p class="loader">Eror...</p>'
+}
+
+const renderUser = (users) => {
+    load.innerHTML = ''
+    const ul = document.createElement('ul')
+    ul.className = 'users'
+
+    users.forEach((user) => {
+        ul.innerHTML += `<li>
+        info: ${user.name.first} ${user.name.last}
+        <br>
+        gender: ${user.gender} 
+        <br>
+        email: ${user.email}
+        <br>
+        </li>`
+        console.log(user);
+    });
+    wrapper.append(ul)
+    const btnSubmit = document.createElement('button')
+    btnSubmit.textContent = 'btn';
+    wrapper.append(btnSubmit)
+    btnSubmit.addEventListener('click', () => {
+    current += 5
+    fetchUsers(`https://randomuser.me/api/?results=${current}`)
+})
+}
+
+const fetchUsers = (url) => {
+    renderLoading()
+
+    fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+        renderUser(data.results)
+    })
+    .catch(() => {
+        renderEror()
+    })
+    
+};
+
+
+
+fetchUsers(`https://randomuser.me/api/?results=${current}`)
