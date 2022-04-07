a484a91fb4b3a4b1a465e60fe58904c7ca31b49c
MaryZaiko <604926@gmail.com>
2022-04-07 09:41:45 +0300

    added btnCommetns

--- lesson-7/classwork2/script.js
+++ lesson-7/classwork2/script.js
@@ -16,25 +16,30 @@
   отправлять за комментариями к посту (https://jsonplaceholder.typicode.com/posts/{{ id }}/comments)
 */
 
-const fetchUrls = [
-  fetch("https://jsonplaceholder.typicode.com/posts"),
-  fetch("https://jsonplaceholder.typicode.com/todos"),
-  fetch("https://jsonplaceholder.typicode.com/users"),
-];
+let activeTabs = null;
 
 let posts = null;
-let todos = null;
 let users = null;
+let todos = null;
 
 let postEnd = 5;
 let userEnd = 5;
 let todoEnd = 5;
+
+let postsFilterValue = "";
+let usersFilterValue = "";
+let todosFilterValue = "";
+
+let sortStatusTodo = "filterByAll";
+
+let postsCommentsById = {};
 
 const divTabs = document.getElementById("tabs");
 const divSearchInput = document.getElementById("inputFilter");
 const divFilterTodo = document.getElementById("btnFilterTodo");
 const divList = document.getElementById("list");
 const divLoadErr = document.getElementById("loadErr");
+const divBtnLoadMore = document.getElementById("loadMore");
 
 const renderLoading = () => {
   divLoadErr.innerHTML = '<span class="loading"> Loading...</span>';
@@ -42,6 +47,8 @@ const renderLoading = () => {
 const renderError = (err) => {
   divLoadErr.innerHTML = `<span class="err"> ${err}</span>`;
 };
+// TABS
+
 const renderTabs = () => {
   const postsBtn = document.createElement("button");
   postsBtn.textContent = "POSTS";
@@ -52,7 +59,6 @@ const renderTabs = () => {
   divTabs.append(postsBtn, todosBtn, usersBtn);
 
   postsBtn.classList.add("btnActive");
-  renderPostList(posts);
 
   postsBtn.addEventListener("click", () => {
     postsBtn.classList.add("btnActive");
@@ -60,6 +66,7 @@ const renderTabs = () => {
     usersBtn.classList.remove("btnActive");
     renderPostList(posts);
     divFilterTodo.innerHTML = "";
+    activeTabs = "posts";
   });
   todosBtn.addEventListener("click", () => {
     postsBtn.classList.remove("btnActive");
@@ -67,6 +74,7 @@ const renderTabs = () => {
     usersBtn.classList.remove("btnActive");
 
     renderTodoList(todos);
+    activeTabs = "todos";
   });
   usersBtn.addEventListener("click", () => {
     postsBtn.classList.remove("btnActive");
@@ -74,66 +82,151 @@ const renderTabs = () => {
     usersBtn.classList.add("btnActive");
     divFilterTodo.innerHTML = "";
     renderUsersList(users);
+    activeTabs = "users";
   });
 };
+
+//INPUT
 
 const renderInput = () => {
   const inputSearch = document.createElement("input");
   inputSearch.classList.add("inputSearch");
   divSearchInput.append(inputSearch);
+
+  inputSearch.addEventListener("input", (event) => {
+    if (activeTabs === "posts") {
+      postsFilterValue = event.target.value;
+      renderPostList(posts);
+    }
+    if (activeTabs === "users") {
+      usersFilterValue = event.target.value;
+      renderUsersList(users);
+    }
+    if (activeTabs === "todos") {
+      todosFilterValue = event.target.value;
+      renderTodoList(todos);
+    }
+  });
+};
+
+//LIST renderPost
+
+const fetchCommentsById = (id) => {
+  renderLoading();
+  fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`)
+    .then((response) => response.json())
+    .then((comments) => {
+    
+      postsCommentsById[id] = comments;
+      renderPostList(posts);
+      divLoadErr.innerHTML = "";
+
+      // return postsCommentsById;
+    })
+    .catch((err) => {
+      renderError(err);
+    });
+};
+
+const renderComments = (data) => {
+  console.log(data);
+
+  data.forEach((item) => console.log(item.name));
+
+  const divComments = document.createElement("div");
+
+  data.forEach((item) => {
+    const h3 = document.createElement("h3");
+    h3.textContent = item.name;
+    const p = document.createElement("p");
+    p.textContent = item.body;
+    divComments.append(h3, p);
+    console.log(divComments);
+    return divComments;
+  });
+};
+
+const renderBtnComments = (data) => {
+  const button = document.createElement("button");
+  button.textContent = "show comments";
+
+  button.addEventListener("click", () => {
+    fetchCommentsById(data.id);
+    console.log(postsCommentsById);
+
+    // renderComments(postsCommentsById);
+  });
+  return button;
 };
 
 const renderPostList = (data) => {
   divList.innerHTML = "";
 
-  let currentData = data.slice(0, `${postEnd}`);
   const ul = document.createElement("ul");
   ul.classList = "posts-list";
   divList.append(ul);
 
-  currentData.forEach((post) => {
-    ul.innerHTML += `<li> <h2>${post.title}</h2> <p>${post.body} </p> </li>`;
-  });
+  data
+    .filter((post) =>
+      post.title.toLowerCase().includes(postsFilterValue.toLowerCase())
+    )
+    .slice(0, postEnd)
+    .forEach((post) => {
+      const li = document.createElement("li");
+      const h2 = document.createElement("h2");
+      h2.textContent = post.title;
+      const p = document.createElement("p");
+      p.textContent = post.body;
+      const divComments = document.createElement("div");
 
-  const btnLoadMorePosts = document.createElement("button");
-  btnLoadMorePosts.textContent = "Load more";
-  divList.append(btnLoadMorePosts);
+      if (postsCommentsById[post.id]) {
+        divComments.append(renderComments(postsCommentsById[post.id]));
+      } else {
+        divComments.append(renderBtnComments(post));
+      }
 
-  // btnLoadMorePosts.addEventListener("click", () => {
-  //   postEnd += 5;
-  //   fetchData();
-  // });
+      li.append(h2, p, divComments);
+      ul.append(li);
+    });
 
-  console.log(data);
+  // console.log(data);
 };
+
+//LIST renderTodo
 
 const renderFilterTodoList = (data) => {
   const ul = document.createElement("ul");
   ul.classList = "todo-list";
   divList.append(ul);
-  data.forEach((todo) => {
-    const li = document.createElement("li");
-    const h2 = document.createElement("h2");
-    h2.textContent = todo.title;
-    const p = document.createElement("p");
-    p.textContent = todo.completed ? "Done" : "Not done";
-    li.append(h2, p);
-    ul.append(li);
 
-    // ul.innerHTML += `<li><h2>Title: ${todo.title}</h2> <p> ${todo.completed} ? Done : none</p></li>`;
-  });
-};
-
-const renderBtnLoadMoreTodos = () => {
-  const btnLoadMoreTodos = document.createElement("button");
-  btnLoadMoreTodos.textContent = "Load more";
-  divList.append(btnLoadMoreTodos);
+  data
+    .filter((todo) => {
+      if (sortStatusTodo === "filterByCompleted") {
+        return (
+          todo.completed &&
+          todo.title.toLowerCase().includes(todosFilterValue.toLowerCase())
+        );
+      }
+      if (sortStatusTodo === "filterByAll") {
+        return todo.title
+          .toLowerCase()
+          .includes(todosFilterValue.toLowerCase());
+      }
+    })
+    .slice(0, todoEnd)
+    .forEach((todo) => {
+      const li = document.createElement("li");
+      const h2 = document.createElement("h2");
+      h2.textContent = todo.title;
+      const p = document.createElement("p");
+      p.textContent = todo.completed ? "Done" : "Not done";
+      li.append(h2, p);
+      ul.append(li);
+    });
 };
 
 const renderTodoList = (data) => {
   divList.innerHTML = "";
-
-  let currentData = data.slice(0, `${todoEnd}`);
 
   const renderBtnFilterTodo = () => {
     divFilterTodo.innerHTML = "";
@@ -144,18 +237,22 @@ const renderTodoList = (data) => {
     btnTodoComplited.textContent = "TODO Complited";
 
     divFilterTodo.append(btnTodoAll, btnTodoComplited);
-    btnTodoAll.classList.add("btnActive");
-    renderFilterTodoList(currentData);
-    renderBtnLoadMoreTodos();
+
+    if (sortStatusTodo === "filterByAll") {
+      btnTodoAll.classList.add("btnActive");
+    } else {
+      btnTodoComplited.classList.add("btnActive");
+    }
+
+    renderFilterTodoList(data);
 
     btnTodoAll.addEventListener("click", () => {
       btnTodoAll.classList.add("btnActive");
       btnTodoComplited.classList.remove("btnActive");
 
       divList.innerHTML = "";
-
-      renderFilterTodoList(currentData);
-      renderBtnLoadMoreTodos();
+      sortStatusTodo = "filterByAll";
+      renderFilterTodoList(data);
     });
 
     btnTodoComplited.addEventListener("click", () => {
@@ -163,49 +260,69 @@ const renderTodoList = (data) => {
       btnTodoAll.classList.remove("btnActive");
 
       divList.innerHTML = "";
-
-      let filterData = currentData.filter((todo) => todo.completed);
-      renderFilterTodoList(filterData);
-      renderBtnLoadMoreTodos();
+      sortStatusTodo = "filterByCompleted";
+      renderFilterTodoList(data);
     });
   };
 
-  console.log(data);
-
   renderBtnFilterTodo();
+  console.log(data);
 };
+
+//LIST renderUsers
 
 const renderUsersList = (data) => {
   divList.innerHTML = "";
 
-  let currentData = data.slice(0, `${userEnd}`);
-  console.log(data);
-
   const ul = document.createElement("ul");
   ul.classList = "users-list";
   divList.append(ul);
-  currentData.forEach((user) => {
-    ul.innerHTML += `<li> <h2>${user.name}</h2> <p>${user.email}</p> <p>Address: ${user.address.city}</p> <p>Company: ${user.company.name}</p>  </li>`;
-  });
 
-  const btnLoadMoreUsers = document.createElement("button");
-  btnLoadMoreUsers.textContent = "Load more";
-  divList.append(btnLoadMoreUsers);
-
-  btnLoadMoreUsers.addEventListener("click", () => {
-    userEnd += 5;
-    renderUsersList(currentData);
-    btnLoadMoreUsers.classList.add("btnActive"); // не применяется стиль из-за renderUsersList(currentData)???
-
-    console.log(currentData);
-    console.log(userEnd);
-  });
+  data
+    .filter((user) =>
+      user.name.toLowerCase().includes(usersFilterValue.toLowerCase())
+    )
+    .slice(0, userEnd)
+    .forEach((user) => {
+      ul.innerHTML += `<li> <h2>${user.name}</h2> <p>${user.email}</p> <p>Address: ${user.address.city}</p> <p>Company: ${user.company.name}</p>  </li>`;
+    });
 
   console.log(data);
 };
 
+//BtnLoadMore
+
+const renderBtnLoadMore = () => {
+  const btnLoadMore = document.createElement("button");
+  btnLoadMore.textContent = "Load More";
+  divBtnLoadMore.append(btnLoadMore);
+
+  btnLoadMore.addEventListener("click", () => {
+    if (activeTabs === "posts") {
+      postEnd += 5;
+      renderPostList(posts);
+    }
+    if (activeTabs === "users") {
+      userEnd += 5;
+      renderUsersList(users);
+    }
+    if (activeTabs === "todos") {
+      todoEnd += 5;
+      renderTodoList(todos);
+    }
+  });
+};
+
+//fetchData
+
 const fetchData = () => {
   renderLoading();
+
+  const fetchUrls = [
+    fetch("https://jsonplaceholder.typicode.com/posts"),
+    fetch("https://jsonplaceholder.typicode.com/todos"),
+    fetch("https://jsonplaceholder.typicode.com/users"),
+  ];
 
   Promise.allSettled(fetchUrls)
     .then((results) => {
@@ -216,13 +333,13 @@ const fetchData = () => {
       todos = res[1];
       users = res[2];
 
+      activeTabs = "posts";
+
       renderTabs();
       renderInput();
-      // renderPostList(posts);
-      // renderUsersList(users);
-      // renderTodoList(todos);
+      renderPostList(posts);
 
-      console.log();
+      renderBtnLoadMore();
       divLoadErr.innerHTML = "";
     })
     .catch((err) => {
